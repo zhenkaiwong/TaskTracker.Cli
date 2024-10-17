cc42b046df3518840975a8118ccdec7af0d9d44e
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-10-18 01:19:07 +0800

    feat: implement data service interface and file storage based json data service

---
+++ DataServices/FileStorageJsonDataService.cs
@@ -0,0 +1,238 @@
+using System.Text.Json;
+using TaskTracker.Cli.Models;
+
+namespace TaskTracker.Cli.DataServices;
+
+public class FileStorageJsonDataService : IDataService
+{
+  protected readonly string _fileName = "data.json";
+
+
+  /// <summary>
+  /// Checks if the datasource file exists.
+  /// </summary>
+  protected bool IsDatasourceExist()
+  {
+    return File.Exists(_fileName);
+  }
+
+  /// <summary>
+  /// Attempts to write the list of tasks to the datasource file.
+  /// </summary>
+  /// <param name="tasks">The list of tasks to be serialized and written to the datasource.</param>
+  /// <param name="error">Outputs the error message if writing to the datasource fails.</param>
+  /// <returns>True if the tasks are successfully written to the datasource, otherwise false.</returns>
+  protected bool TryWriteTasksToDatasource(List<Models.Task> tasks, out string error)
+  {
+    try
+    {
+      var options = new JsonSerializerOptions { WriteIndented = true };
+      string tasksJson = JsonSerializer.Serialize(tasks, options);
+      File.WriteAllText(_fileName, tasksJson);
+      error = string.Empty;
+      return true;
+    }
+    catch (Exception e)
+    {
+      error = e.Message;
+      return false;
+    }
+  }
+
+  /// <summary>
+  /// Try to initialize the datasource file regardless of its existence
+  /// </summary>
+  /// <param name="error">The error message if the datasource file cannot be 
+  /// initialized</param>
+  /// <returns>True if the datasource file is successfully initialized, else 
+  /// false</returns>
+  protected bool TryInitializeDatasourceFile(out string error)
+  {
+    try
+    {
+      File.WriteAllText(_fileName, "[]");
+    }
+    catch (Exception e)
+    {
+      error = e.Message;
+      return false;
+    }
+    error = string.Empty;
+    return true;
+  }
+
+  /// <summary>
+  /// Try to get the total task count from the datasource
+  /// </summary>
+  /// <param name="taskCount">The total task count if the datasource file can be read
+  /// successfully</param>
+  /// <param name="error">The error message if the task count cannot be retrieved</param>
+  /// <returns>True if the task count can be retrieved, else false</returns>
+  public bool TryGetTaskCountFromDatasource(out int taskCount, out string error)
+  {
+    var getTasksSuccess = TryGetAllTasks(out var tasks, out error);
+    if (!getTasksSuccess)
+    {
+      taskCount = 0;
+      return false;
+    }
+
+    taskCount = tasks.Count;
+    return true;
+  }
+  /// <summary>
+  /// Attempts to retrieve all tasks from the datasource file. If datasource doesn't exist, an empty list is returned.
+  /// </summary>
+  /// <param name="tasks">The list that will be populated with tasks retrieved from the datasource.</param>
+  /// <param name="error">An error message if the operation fails; otherwise, an empty string.</param>
+  /// <returns>True if all tasks are successfully retrieved and deserialized; otherwise, false.</returns>
+  public bool TryGetAllTasks(out List<Models.Task> tasks, out string error)
+  {
+    tasks = new List<Models.Task>();
+    error = string.Empty;
+
+    if (!IsDatasourceExist())
+    {
+      error = string.Empty;
+      return true;
+    }
+
+    var tasksFromDatasource = JsonSerializer.Deserialize<List<TaskModel>>(File.ReadAllText(_fileName));
+
+    if (tasksFromDatasource is null)
+    {
+      error = "Unable to read tasks from datasource";
+      return false;
+    }
+
+    if (tasksFromDatasource.Count == 0) return true;
+
+    foreach (var t in tasksFromDatasource)
+    {
+      var createTaskSuccess = Models.Task.TryCreate(t.Id, t.Description, t.Status, t.CreatedAt, t.UpdatedAt, out var task, out error);
+      if (!createTaskSuccess)
+      {
+        return false;
+      }
+
+      if (task is null)
+      {
+        error = $"Unable to map task from datasource. Problematic task: {t.Description}";
+        return false;
+      }
+
+      tasks.Add(task);
+    }
+
+    return true;
+  }
+  /// <summary>
+  /// Attempts to insert a task into the datasource.
+  /// </summary>
+  /// <param name="task">The task to be inserted.</param>
+  /// <param name="error">Outputs the error message if the insertion fails.</param>
+  /// <returns>True if the task is successfully inserted into the datasource, otherwise false.</returns>
+  public bool TryInsertTask(Models.Task task, out string error)
+  {
+    var getTasksSuccess = TryGetAllTasks(out var tasks, out error);
+    if (!getTasksSuccess)
+    {
+      return false;
+    }
+
+    tasks.Add(task);
+
+    var writeTasksSuccess = TryWriteTasksToDatasource(tasks, out error);
+    if (!writeTasksSuccess)
+    {
+      return false;
+    }
+
+    return true;
+  }
+  /// <summary>
+  /// Attempts to retrieve a task from the datasource by its ID.
+  /// </summary>
+  /// <param name="id">The ID of the task to retrieve</param>
+  /// <param name="task">The retrieved task if the retrieval is successful</param>
+  /// <param name="error">The error message if the retrieval fails</param>
+  /// <returns>True if the retrieval is successful, otherwise false</returns>
+  public bool TryGetTask(int id, out Models.Task? task, out string error)
+  {
+    task = null;
+    var getTasksSuccess = TryGetAllTasks(out var tasks, out error);
+    if (!getTasksSuccess)
+    {
+      return false;
+    }
+
+    foreach (var t in tasks)
+    {
+      if (t.Id == id)
+      {
+        task = t;
+        return true;
+      }
+    }
+
+    error = $"Unable to find task with ID {id}";
+    return false;
+  }
+  /// <summary>
+  /// Attempts to update an existing task in the datasource.
+  /// </summary>
+  /// <param name="task">The task object containing updated information.</param>
+  /// <param name="error">The error message if the task cannot be updated.</param>
+  /// <returns>True if the task is successfully updated, otherwise false.</returns>
+  public bool TryUpdateTask(Models.Task task, out string error)
+  {
+    var getTasksSuccess = TryGetAllTasks(out var tasks, out error);
+    if (!getTasksSuccess)
+    {
+      return false;
+    }
+
+    foreach (var t in tasks)
+    {
+      if (t.Id == task.Id)
+      {
+        var updateTaskStatusSuccess = t.TryUpdateStatus(task.Status, out error);
+        if (!updateTaskStatusSuccess)
+        {
+          return false;
+        }
+        t.Description = task.Description;
+        t.UpdatedAt = task.UpdatedAt;
+        break;
+      }
+    }
+
+    return TryWriteTasksToDatasource(tasks, out error);
+  }
+  /// <summary>
+  /// Attempts to remove a task with the given ID from the datasource and save the updated list of tasks to the datasource.
+  /// </summary>
+  /// <param name="id">The ID of the task to be deleted.</param>
+  /// <param name="error">The error message if the deletion fails.</param>
+  /// <returns>True if the task is successfully deleted, otherwise false.</returns>
+  public bool TryDeleteTask(int id, out string error)
+  {
+    var getTasksSuccess = TryGetAllTasks(out var tasks, out error);
+    if (!getTasksSuccess)
+    {
+      return false;
+    }
+
+    foreach (var t in tasks)
+    {
+      if (t.Id == id)
+      {
+        tasks.Remove(t);
+        break;
+      }
+    }
+
+    return TryWriteTasksToDatasource(tasks, out error);
+  }
+
+}
---
+++ DataServices/IDataService.cs
@@ -0,0 +1,11 @@
+namespace TaskTracker.Cli.DataServices;
+
+public interface IDataService
+{
+  public bool TryInsertTask(Models.Task task, out string error);
+  public bool TryGetAllTasks(out List<Models.Task> tasks, out string error);
+  public bool TryGetTask(int id, out Models.Task? task, out string error);
+  public bool TryUpdateTask(Models.Task task, out string error);
+  public bool TryDeleteTask(int id, out string error);
+  public bool TryGetTaskCountFromDatasource(out int taskCount, out string error);
+}
