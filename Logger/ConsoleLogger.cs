1bae0ff1cb2c72d48abb88812681f720c197954a
zhenkaiwong1996 <zhenkaiwong.work@gmail.com>
2024-10-18 00:57:03 +0800

    chore: Move documentation from abstract method to implemented method

--- Logger/ConsoleLogger.cs
+++ Logger/ConsoleLogger.cs
@@ -4,11 +4,20 @@ namespace TaskTracker.Cli.Logger;
 
 public class ConsoleLogger : BaseLogger
 {
+  /// <summary>
+  /// Logs an debug message with the provided message and message source (an object).
+  /// </summary>
+  /// <param name="message">The debug message to be logged</param>
+  /// <param name="messageSource">The source of the debug message (the object that invoke this method)</param>
   public override void Debug(string debugMessage, object messageSource)
   {
     Debug(debugMessage, messageSource.GetType().Name);
   }
-
+  /// <summary>
+  /// Logs an debug message with the provided message and message source.
+  /// </summary>
+  /// <param name="message">The debug message to be logged</param>
+  /// <param name="messageSource">The source of the debug message</param>
   public override void Debug(string debugMessage, string messageSourceName)
   {
     if (!IsDebugModeOn()) return;
@@ -16,24 +25,40 @@ public class ConsoleLogger : BaseLogger
     Console.WriteLine($"DEBUG: {debugMessage} [{messageSourceName}]");
     Console.ResetColor();
   }
-
+  /// <summary>
+  /// Logs an error message with the provided message and message source (an object).
+  /// </summary>
+  /// <param name="message">The error message to be logged</param>
+  /// <param name="messageSource">The source of the error message (the object that invoke this method)</param>
   public override void Error(string errorMessage, object messageSource)
   {
     Error(errorMessage, messageSource.GetType().Name);
   }
-
+  /// <summary>
+  /// Logs an error message with the provided message and message source.
+  /// </summary>
+  /// <param name="message">The error message to be logged</param>
+  /// <param name="messageSource">The source of the error message</param>
   public override void Error(string errorMessage, string messageSourceName)
   {
     Console.ForegroundColor = ConsoleColor.Red;
     Console.WriteLine($"ERROR: {errorMessage} [{messageSourceName}]");
     Console.ResetColor();
   }
-
+  /// <summary>
+  /// Logs an informational message with the provided message and message source (an object).
+  /// </summary>
+  /// <param name="message">The message to be logged</param>
+  /// <param name="messageSource">The source of the message (the object that invoke this method)</param>
   public override void Info(string message, object messageSource)
   {
     Info(message, messageSource.GetType().Name);
   }
-
+  /// <summary>
+  /// Logs an informational message with the provided message and message source.
+  /// </summary>
+  /// <param name="message">The message to be logged</param>
+  /// <param name="messageSource">The source of the message</param>
   public override void Info(string message, string messageSourceName)
   {
     Console.ForegroundColor = ConsoleColor.Green;
